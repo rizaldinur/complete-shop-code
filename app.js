6940d6a652cb5492306a470dabe67c737decd8e0
rizaldinur <rnur71@gmail.com>
2024-10-08 20:21:25 +0700

    10-Setup DB PostgreSQL and test connection

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import adminRoutes from "./routes/admin.js";
 import shopRoutes from "./routes/shop.js";
 import pageNotFound from "./controllers/error.js";
+import db from "./util/dbconfig.js";
 
 const app = express();
 
@@ -14,6 +15,10 @@ app.set("views", "views");
 console.log(rootDir);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, "public")));
+
+db.query("SELECT * FROM products").then((result) => {
+  console.log(result);
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
