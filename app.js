22fed096f027303b774231dfa19ce87e3ebcb8df
rizaldinur <rnur71@gmail.com>
2024-10-10 13:49:19 +0700

    11-Create User model and manage dummy user
    Add one to many association with Product model. Find or create(if not exist) a user and use it as dummy user to handle other functions.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import adminRoutes from "./routes/admin.js";
 import shopRoutes from "./routes/shop.js";
 import pageNotFound from "./controllers/error.js";
 import sequelize from "./util/dbconfig.js";
+import Product from "./models/product.js";
+import User from "./models/user.js";
 
 const app = express();
 
@@ -16,24 +18,45 @@ console.log(rootDir);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, "public")));
 
+//store dummy user data in request
+app.use(async (req, res, next) => {
+  const user = await User.findByPk(1);
+  req.user = user;
+  console.log(req.user);
+  next();
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(pageNotFound);
 
-app.listen(3000, async () => {
-  console.log(`server running on http://localhost:3000`);
-  try {
-    await sequelize.authenticate();
-    console.log("DB Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
+// test DB connection
+try {
+  await sequelize.authenticate();
+  console.log("DB Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
 
-  try {
-    await sequelize.sync();
-    console.log("DB Synced.");
-  } catch (error) {
-    console.error("Failed to sync DB:", error);
-  }
-});
+// initialize DB synchronization
+try {
+  User.hasMany(Product);
+  Product.belongsTo(User);
+  await sequelize.sync();
+  console.log("DB Synced.");
+  const [user, created] = await User.findOrCreate({
+    where: { id: 1 },
+    defaults: {
+      name: "Skibidi",
+      email: "test@test.com",
+    },
+  });
+  console.log("Is Created? ", created);
+
+  app.listen(3000, () => {
+    console.log(`server running on http://localhost:3000`);
+  });
+} catch (error) {
+  console.error("Failed to sync DB:", error);
+}
---
+++ models/user.js
@@ -0,0 +1,15 @@
+import { Sequelize, DataTypes } from "sequelize";
+import sequelize from "../util/dbconfig.js";
+
+const User = sequelize.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: DataTypes.STRING,
+  email: DataTypes.STRING,
+});
+
+export default User;
