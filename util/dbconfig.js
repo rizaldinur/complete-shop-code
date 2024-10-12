0658b1affc41f93cb11a931f55596ede0a90a226
rizaldinur <rnur71@gmail.com>
2024-10-12 17:47:14 +0700

    12-set up mongodb connection

--- util/dbconfig.js
+++ util/dbconfig.js
@@ -1,10 +1,19 @@
-import dotenv from "dotenv";
-import { Sequelize } from "sequelize";
+import { config } from "dotenv";
+import mongodb from "mongodb";
 
-dotenv.config();
+config();
 
-const sequelize = new Sequelize(process.env.DBConnLink, {
-  dialect: "postgres",
-});
+let _db;
+const MongoClient = mongodb.MongoClient;
 
-export default sequelize;
+export const mongoConnect = async () => {
+  const client = await MongoClient.connect(process.env.DATABASE_URL);
+  _db = client.db("shop");
+};
+
+export const getDB = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No DB found!";
+};
