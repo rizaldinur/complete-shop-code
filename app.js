6d938d814dad3e2ad2237b953b461d0534f0188b
rizaldinur <rnur71@gmail.com>
2024-10-12 17:48:27 +0700

    12-Connect to mongo client in app.js

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ import bodyParser from "body-parser";
 import adminRoutes from "./routes/admin.js";
 import shopRoutes from "./routes/shop.js";
 import pageNotFound from "./controllers/error.js";
-import sequelize from "./util/dbconfig.js";
-import Product from "./models/product.js";
-import User from "./models/user.js";
-import Cart from "./models/cart.js";
-import CartItem from "./models/cart-item.js";
-import Order from "./models/order.js";
-import OrderItem from "./models/order-item.js";
+// // import Product from "./models/product.js";
+// import User from "./models/user.js";
+// import Cart from "./models/cart.js";
+// import CartItem from "./models/cart-item.js";
+// import Order from "./models/order.js";
+// import OrderItem from "./models/order-item.js";
+import { getDB, mongoConnect } from "./util/dbconfig.js";
 
 const app = express();
 
@@ -25,8 +25,8 @@ app.use(express.static(path.join(rootDir, "public")));
 //store dummy user data as request
 //when request comes through this middleware
 app.use(async (req, res, next) => {
-  const user = await User.findByPk(1);
-  req.user = user;
+  // const user = await User.findByPk(1);
+  // req.user = user;
   next();
 });
 
@@ -35,50 +35,60 @@ app.use(shopRoutes);
 
 app.use(pageNotFound);
 
-// test DB connection
+// // test DB connection
+// try {
+//   await sequelize.authenticate();
+//   console.log("DB Connection has been established successfully.");
+// } catch (error) {
+//   console.error("Unable to connect to the database:", error);
+// }
+
+// // initialize DB synchronization
+// try {
+//   //1 to many
+//   User.hasMany(Product, { onDelete: "CASCADE" });
+//   Product.belongsTo(User);
+
+//   User.hasMany(Order);
+//   Order.belongsTo(User);
+//   //1 to 1
+//   User.hasOne(Cart, { foreignKey: { unique: true } });
+//   Cart.belongsTo(User);
+//   //many to many
+//   Cart.belongsToMany(Product, { through: CartItem });
+//   Product.belongsToMany(Cart, { through: CartItem });
+//   //n:m Order-Product
+//   Order.belongsToMany(Product, { through: OrderItem });
+//   Product.belongsToMany(Order, { through: OrderItem });
+
+//   // await sequelize.sync({ force: true });
+//   await sequelize.sync();
+//   console.log("DB Synced.");
+//   const [user, created] = await User.findOrCreate({
+//     where: { id: 1 },
+//     defaults: {
+//       name: "Skibidi",
+//       email: "test@test.com",
+//     },
+//   });
+//   console.log("Is User Created? ", created);
+//   if (!(await user.getCart())) {
+//     await user.createCart();
+//   }
+
+//   app.listen(3000, () => {
+//     console.log(`server running on http://localhost:3000`);
+//   });
+// } catch (error) {
+//   console.error("Failed to sync DB:", error);
+// }
+
 try {
-  await sequelize.authenticate();
-  console.log("DB Connection has been established successfully.");
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
-}
-
-// initialize DB synchronization
-try {
-  //1 to many
-  User.hasMany(Product, { onDelete: "CASCADE" });
-  Product.belongsTo(User);
-
-  User.hasMany(Order);
-  Order.belongsTo(User);
-  //1 to 1
-  User.hasOne(Cart, { foreignKey: { unique: true } });
-  Cart.belongsTo(User);
-  //many to many
-  Cart.belongsToMany(Product, { through: CartItem });
-  Product.belongsToMany(Cart, { through: CartItem });
-  //n:m Order-Product
-  Order.belongsToMany(Product, { through: OrderItem });
-  Product.belongsToMany(Order, { through: OrderItem });
-
-  // await sequelize.sync({ force: true });
-  await sequelize.sync();
-  console.log("DB Synced.");
-  const [user, created] = await User.findOrCreate({
-    where: { id: 1 },
-    defaults: {
-      name: "Skibidi",
-      email: "test@test.com",
-    },
-  });
-  console.log("Is User Created? ", created);
-  if (!(await user.getCart())) {
-    await user.createCart();
-  }
-
+  await mongoConnect();
+  console.log("Connected to Mongo Client!");
   app.listen(3000, () => {
     console.log(`server running on http://localhost:3000`);
   });
 } catch (error) {
-  console.error("Failed to sync DB:", error);
+  console.log(error);
 }
