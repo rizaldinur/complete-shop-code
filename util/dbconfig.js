a153bb68913b8045282c2dd9f3eeffac41839726
rizaldinur <rnur71@gmail.com>
2024-11-05 12:00:59 +0700

    19-modify db conn timeout

--- util/dbconfig.js
+++ util/dbconfig.js
@@ -5,5 +5,7 @@ import mongoose from "mongoose";
 config();
 
 export const mongooseConnect = async () => {
-  await mongoose.connect(process.env.DATABASE_URL);
+  await mongoose.connect(process.env.DATABASE_URL, {
+    serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
+  });
 };
